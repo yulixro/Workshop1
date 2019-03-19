f8df8e44ff9a7357706a49cb569758349f53deda
chris_cy_wu <chris_cy_wu@gss.com.tw>
2019-03-19 17:35:25 +0800

    add basic file

---
+++ Workshop1/js/script.js
@@ -0,0 +1,22 @@
+
+var bookDataFromLocalStorage = [];
+var bookCategoryList = [
+    { text: "資料庫", value: "database", src: "image/database.jpg" },
+    { text: "網際網路", value: "internet", src: "image/internet.jpg" },
+    { text: "應用系統整合", value: "system", src: "image/system.jpg" },
+    { text: "家庭保健", value: "home", src: "image/home.jpg" },
+    { text: "語言", value: "language", src: "image/language.jpg" }
+];
+
+// 載入書籍資料
+function loadBookData() {
+    bookDataFromLocalStorage = JSON.parse(localStorage.getItem('bookData'));
+    if (bookDataFromLocalStorage == null) {
+        bookDataFromLocalStorage = bookData;
+        localStorage.setItem('bookData', JSON.stringify(bookDataFromLocalStorage));
+    }
+}
+
+$(function () {
+    loadBookData();
+});
